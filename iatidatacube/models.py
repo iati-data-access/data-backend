d2de2d513e9d924068df64183fedefb4e82d57f1
Mark Brough <mark.brough@gmail.com>
2023-08-08 16:58:26 +0200

    Update model with changes to provider and receiver organisations; keeping redundant data columns for now

--- iatidatacube/models.py
+++ iatidatacube/models.py
@@ -176,13 +176,53 @@ class Dataset(db.Model):
     status = sa.Column(sa.Integer, nullable=False, index=True)
 
 
+class ProviderOrganisation(db.Model):
+    __tablename__="provider_organisation"
+    id = sa.Column(sa.UnicodeText, primary_key=True)
+    name_en = sa.Column(sa.UnicodeText,
+        nullable=False)
+    name_fr = sa.Column(sa.UnicodeText,
+        nullable=False)
+    name_pt = sa.Column(sa.UnicodeText,
+        nullable=False)
+    name_es = sa.Column(sa.UnicodeText,
+        nullable=False)
+
+    __table_args__ = (
+        sa.Index('ix_provider_organisation_name_en', sa.func.LEFT(name_en, 100)),
+        sa.Index('ix_provider_organisation_name_fr', sa.func.LEFT(name_fr, 100)),
+        sa.Index('ix_provider_organisation_name_pt', sa.func.LEFT(name_pt, 100)),
+        sa.Index('ix_provider_organisation_name_es', sa.func.LEFT(name_es, 100)),
+    )
+
+
+class ReceiverOrganisation(db.Model):
+    __tablename__="receiver_organisation"
+    id = sa.Column(sa.UnicodeText, primary_key=True)
+    name_en = sa.Column(sa.UnicodeText,
+        nullable=False)
+    name_fr = sa.Column(sa.UnicodeText,
+        nullable=False)
+    name_pt = sa.Column(sa.UnicodeText,
+        nullable=False)
+    name_es = sa.Column(sa.UnicodeText,
+        nullable=False)
+
+    __table_args__ = (
+        sa.Index('ix_receiver_organisation_name_en', sa.text("left(name_en, 100)")),
+        sa.Index('ix_receiver_organisation_name_fr', sa.text("left(name_fr, 100)")),
+        sa.Index('ix_receiver_organisation_name_pt', sa.text("left(name_pt, 100)")),
+        sa.Index('ix_receiver_organisation_name_es', sa.text("left(name_es, 100)")),
+    )
+
+
 class IATIActivity(db.Model):
     __tablename__="iati_activity"
     iati_identifier = sa.Column(sa.UnicodeText, primary_key=True)
-    title = sa.Column(sa.UnicodeText, nullable=False, index=True)
-    title_fr = sa.Column(sa.UnicodeText, nullable=False, index=True)
-    title_pt = sa.Column(sa.UnicodeText, nullable=False, index=True)
-    title_es = sa.Column(sa.UnicodeText, nullable=False, index=True)
+    title = sa.Column(sa.UnicodeText, nullable=False)
+    title_fr = sa.Column(sa.UnicodeText, nullable=False)
+    title_pt = sa.Column(sa.UnicodeText, nullable=False)
+    title_es = sa.Column(sa.UnicodeText, nullable=False)
     description = sa.Column(sa.UnicodeText, nullable=False)
     description_fr = sa.Column(sa.UnicodeText, nullable=False)
     description_pt = sa.Column(sa.UnicodeText, nullable=False)
@@ -193,11 +233,19 @@ class IATIActivity(db.Model):
     hrp = sa.Column(sa.UnicodeText, nullable=True, index=True)
     location = sa.Column(sa.UnicodeText, nullable=True)
     _hash = sa.Column(sa.UnicodeText, nullable=True, index=True)
+    reporting_organisation = sa.Column(sa.UnicodeText, nullable=True, index=True)
+
+    __table_args__ = (
+        sa.Index('ix_iati_activity_title', sa.text("left(title, 100)")),
+        sa.Index('ix_iati_activity_title_fr', sa.text("left(title_fr, 100)")),
+        sa.Index('ix_iati_activity_title_es', sa.text("left(title_pt, 100)")),
+        sa.Index('ix_iati_activity_title_pt', sa.text("left(title_es, 100)")),
+    )
 
 
 class IATILine(db.Model):
     __tablename__="iati_line"
-    id = sa.Column(sa.Integer, primary_key=True)
+    id = sa.Column(sa.UnicodeText, primary_key=True)
     iati_identifier = sa.Column(sa.UnicodeText,
         act_ForeignKey("iati_activity.iati_identifier"))
     reporting_organisation_group = sa.Column(
@@ -221,6 +269,9 @@ class IATILine(db.Model):
     flow_type = sa.Column(sa.UnicodeText,
         act_ForeignKey("flow_type.code"),
         nullable=True, index=True)
+    provider_organisation_id = sa.Column(sa.UnicodeText,
+        act_ForeignKey("provider_organisation.id"),
+        nullable=True, index=True)
     provider_organisation = sa.Column(sa.UnicodeText,
         nullable=False)
     provider_organisation_fr = sa.Column(sa.UnicodeText,
@@ -232,6 +283,9 @@ class IATILine(db.Model):
     provider_organisation_type = sa.Column(
         sa.UnicodeText,
         act_ForeignKey("provider_organisation_type.code"),
+        nullable=True, index=True)
+    receiver_organisation_id = sa.Column(sa.UnicodeText,
+        act_ForeignKey("receiver_organisation.id"),
         nullable=True, index=True)
     receiver_organisation = sa.Column(sa.UnicodeText,
         nullable=False)
