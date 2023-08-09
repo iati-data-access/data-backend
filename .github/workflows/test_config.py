92594218e24877456f3e5a0d0d8e52e89d3ef373
Mark Brough <mark.brough@gmail.com>
2023-08-09 20:37:47 +0200

    Add CI for tests

---
+++ .github/workflows/test_config.py
@@ -0,0 +1,5 @@
+SQLALCHEMY_DATABASE_URI = 'postgresql://localhost/iatidatacubetest'
+SQLALCHEMY_TRACK_MODIFICATIONS = False
+FLASK_ENV='development'
+FLASK_DEBUG=False
+BABBAGE_PAGE_MAX=1048575 # Excel maximum rows (-1 for the header)
